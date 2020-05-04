ede272dcbc5c05f2c3a4fa4e3e4f49cb1a1d2724
Cindy Li <cindyli0213@hotmail.com>
2020-05-04 18:11:04 -0400

    Add feedback and covid data db logging

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# MaskDistributor
+# Hospital Predictor
+
+Hospital Predictor is a website which aims to collect and visualize Canadian hospital occupancy data in light of the COVID-19 outbreak.
+
+This project is still a work in progress and can be accessed at [hospitalpredictor.ca](https://www.hospitalpredictor.ca).
+
+Hospital beds data is provided by the [Canadian Institue for Health and Information](cihi.ca/en/covid-19-resources).
+
+## Contributors
+**Dhriti Aravind** - [deestroy](https://github.com/deestroy)
+* Website UI/UX design
+
+**Cindy Li** - [cindyli-13](https://github.com/cindyli-13)
+* Backend and Google Maps API integration
--- src/app.py
+++ src/app.py
@@ -44,83 +44,92 @@ def error_page():
 # update hospital data
 @app.route('/update-hospital-data', methods = ['POST'])
 def update_hospital_data():
-    
-    if request.method == 'POST':
-        try:
-            data = request.get_json()
+    try:
+        data = request.get_json()
 
-            # if health care worker: get hospital and get rating
-            # if general public and been to hospital: get hospital and get rating
-            # if covid: get postal code
-            
-            questions = data['form_response']['definition']['fields']
-            answers = data['form_response']['answers']
-
-            qa_pairs = {}
-
-            for q, a in zip(questions, answers):
-                qa_pairs[q['title']] = a[a['type']]['label'] if a['type'] == 'choice' else a[a['type']]
-
-            # health care worker
-            if qa_pairs['Which best describes you?'] == 'Health Care Worker':
-                hospital = qa_pairs['Which hospital do you work at?']
-                rating = qa_pairs['On a scale of 0 to 10, how would you describe the current situation at your hospital?']
-                scale = 2
-
-            # general public
-            elif qa_pairs['Have you been in a hospital in the last 24 hours?'] == 'Yes':
-                hospital = qa_pairs['Which hospital are you at / have you visited?']
-                rating = qa_pairs['On a scale of 0 to 10, how would you describe the current situation at your hospital?']
-                scale = 1
-
-            # has covid
-            # WHAT TO DO WITH THIS DATA?
-            if qa_pairs['Do you have COVID-19?'] == 'Yes' and 'What is your Postal Code?' in qa_pairs:
-                postal_code = qa_pairs['What is your Postal Code?']
-
-
-            # update occupancy field
-            if scale > 0 and hospital != 'Other':
-                try:
-                    h = db.collection(u'hospitals').document(hospital).get().to_dict()
-                    num_responses = h['num_responses']
-                    curr_occupancy = max(0, h['percent_occupancy'])
-                    updated_occupancy = round((curr_occupancy + rating*10*scale) / (num_responses + scale))
-                    db.collection(u'hospitals').document(hospital).update({u'num_responses': num_responses+1, u'percent_occupancy': updated_occupancy})
-                except:
-                    return jsonify({'Error': 'Hospital does not exist in database'}), 400
-
-            # FOR TESTING PURPOSES
-            elif scale > 0 and hospital == 'Other':
-                try:
-                    h = db.collection(u'ontario_cases').document(u'0').get().to_dict()
-                    num_responses = h['num_responses']
-                    curr_occupancy = max(0, h['percent_occupancy'])
-                    updated_occupancy = round((curr_occupancy + rating*10*scale) / (num_responses + scale))
-                    db.collection(u'ontario_cases').document(u'0').update({u'num_responses': num_responses+1, u'percent_occupancy': updated_occupancy})
-                except:
-                    return jsonify({'Error': 'DB testing failed'}), 400
-
-            return jsonify({'Success':True}), 200
+        # if health care worker: get hospital and get rating
+        # if general public and been to hospital: get hospital and get rating
+        # if covid: get postal code
         
-        except:
-            return jsonify({'Error': 'Something went wrong'}), 400
+        timestamp = data['form_response']['submitted_at']
+        questions = data['form_response']['definition']['fields']
+        answers = data['form_response']['answers']
+
+        qa_pairs = {}
+
+        for q, a in zip(questions, answers):
+            qa_pairs[q['title']] = a[a['type']]['label'] if a['type'] == 'choice' else a[a['type']]
+
+        scale, hospital = -1, -1
+
+        # health care worker
+        if qa_pairs['Which best describes you?'] == 'Health Care Worker':
+            hospital = qa_pairs['Which hospital do you work at?']
+            rating = qa_pairs['On a scale of 0 to 10, how would you describe the current situation at your hospital?']
+            scale = 2
+
+        # general public
+        elif qa_pairs['Have you been in a hospital in the last 24 hours?'] == 'Yes':
+            hospital = qa_pairs['Which hospital are you at / have you visited?']
+            rating = qa_pairs['On a scale of 0 to 10, how would you describe the current situation at your hospital?']
+            scale = 1
+
+        # has covid
+        if qa_pairs['Do you have COVID-19?'] == 'Yes' and 'What is your Postal Code?' in qa_pairs:
+            try:
+                postal_code = qa_pairs['What is your Postal Code?']
+                id = db.collection(u'covid_collected_data').document(u'size').get().to_dict()['value']
+                db.collection(u'covid_collected_data').document(str(id)).set({u'postal_code': postal_code, u'timestamp': timestamp})
+                db.collection(u'covid_collected_data').document(u'size').update({u'size': id+1})
+            except:
+                return jsonify({'Error': 'DB error within collection "covid_collected_data"'}), 400
+
+        # feedback
+        if 'Is there any other data you would like to see on this website?' in qa_pairs:
+            try:
+                feedback = qa_pairs['Is there any other data you would like to see on this website?']
+                id = db.collection(u'feedback').document(u'size').get().to_dict()['value']
+                db.collection(u'feedback').document(str(id)).set({u'feedback': feedback, u'timestamp': timestamp})
+                db.collection(u'feedback').document(u'size').update({u'size': id+1})
+            except:
+                return jsonify({'Error': 'DB error within collection "feedback"'}), 400
+
+        # update occupancy field for corresponding hospital on firestore
+        if scale > 0 and hospital != 'Other':
+            try:
+                h = db.collection(u'hospitals').document(hospital).get().to_dict()
+                num_responses = h['num_responses']
+                curr_occupancy = max(0, h['percent_occupancy'])
+                updated_occupancy = round((curr_occupancy + rating*10*scale) / (num_responses + scale))
+                db.collection(u'hospitals').document(hospital).update({u'num_responses': num_responses+1, u'percent_occupancy': updated_occupancy})
+            except:
+                return jsonify({'Error': 'Hospital does not exist in database'}), 400
+
+        # for testing purposes
+        elif scale > 0 and hospital == 'Other':
+            try:
+                h = db.collection(u'ontario_cases').document(u'0').get().to_dict()
+                num_responses = h['num_responses']
+                curr_occupancy = max(0, h['percent_occupancy'])
+                updated_occupancy = round((curr_occupancy + rating*10*scale) / (num_responses + scale))
+                db.collection(u'ontario_cases').document(u'0').update({u'num_responses': num_responses+1, u'percent_occupancy': updated_occupancy})
+            except:
+                return jsonify({'Error': 'DB testing failed'}), 400
+
+        return jsonify({'Success': True}), 200
     
-    return jsonify({'Error': 'Method not allowed (expected POST)'}), 405
+    except:
+        return jsonify({'Error': 'Incorrect Typeform payload format'}), 400
 
 
 # get all hospital data from database
 @app.route('/get-hospital-data', methods = ['GET'])
 def get_hospital_data():
-
-    if request.method == 'GET':
-        global data
-        if data is None: 
-            print('Performed db query')
-            data = db_query()
-        return json.dumps(data)
-
-    return redirect(url_for('error_page'))
+    global data
+    if data is None: 
+        data = db_query()
+        print('Performed db query') # for testing
+    return json.dumps(data)
 
 
 # retrieve data from db
